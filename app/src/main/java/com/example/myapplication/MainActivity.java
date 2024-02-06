d08378b2fa53b1704cd4b15ec5cb9698f66f936b
spovondra <spovondra@gmail.com>
2024-02-06 23:40:16 +0100

    v4

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,11 +16,6 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     private TextView dateTextView;
-    private CheckBox modeDarkDate;
-    private CheckBox modeCustomAccentDate;
-    private CheckBox dismissDate;
-    private CheckBox limitSelectableDays;
-    private CheckBox highlightDays;
     private DatePickerDialog dpd;
 
     @Override
@@ -30,62 +25,49 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
 
         // Find our View instances
         dateTextView = findViewById(R.id.date_textview);
-        Button dateButton = findViewById(R.id.date_button);
-        modeDarkDate = findViewById(R.id.mode_dark_date);
-        modeCustomAccentDate = findViewById(R.id.mode_custom_accent_date);
-        dismissDate = findViewById(R.id.dismiss_date);
-        limitSelectableDays = findViewById(R.id.limit_dates);
-        highlightDays = findViewById(R.id.highlight_dates);
 
-        // Show a datepicker when the dateButton is clicked
-        dateButton.setOnClickListener(v -> {
-            Calendar now = Calendar.getInstance();
+        // Initialize the date picker dialog
+        initializeDatePicker();
 
-            if (dpd == null) {
-                dpd = DatePickerDialog.newInstance(
-                        MainActivity.this,
-                        now.get(Calendar.YEAR),
-                        now.get(Calendar.MONTH),
-                        now.get(Calendar.DAY_OF_MONTH)
-                );
-            } else {
-                dpd.initialize(
-                        MainActivity.this,
-                        now.get(Calendar.YEAR),
-                        now.get(Calendar.MONTH),
-                        now.get(Calendar.DAY_OF_MONTH)
-                );
+        showDatePicker();
+    }
+
+    // Method to initialize the date picker dialog
+    private void initializeDatePicker() {
+        Calendar now = Calendar.getInstance();
+
+        dpd = DatePickerDialog.newInstance(
+                MainActivity.this,
+                now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH)
+        );
+    }
+
+    // Method to show the date picker dialog
+    private void showDatePicker() {
+        if (dpd != null) {
+            dpd.setThemeDark(false);
+            dpd.setAccentColor(Color.parseColor("#9C27B0"));
+            Calendar[] days = new Calendar[13];
+            for (int i = -6; i < 7; i++) {
+                Calendar day = Calendar.getInstance();
+                day.add(Calendar.DAY_OF_MONTH, i * 2);
+                days[i + 6] = day;
             }
-            dpd.setThemeDark(modeDarkDate.isChecked());
-            dpd.dismissOnPause(dismissDate.isChecked());
-            if (modeCustomAccentDate.isChecked()) {
-                dpd.setAccentColor(Color.parseColor("#9C27B0"));
-            }
-            if (highlightDays.isChecked()) {
-                Calendar date1 = Calendar.getInstance();
-                Calendar date2 = Calendar.getInstance();
-                date2.add(Calendar.WEEK_OF_MONTH, -1);
-                Calendar date3 = Calendar.getInstance();
-                date3.add(Calendar.WEEK_OF_MONTH, 1);
-                Calendar[] days = {date1, date2, date3};
-                dpd.setHighlightedDays(days);
-            }
-            if (limitSelectableDays.isChecked()) {
-                Calendar[] days = new Calendar[13];
-                for (int i = -6; i < 7; i++) {
-                    Calendar day = Calendar.getInstance();
-                    day.add(Calendar.DAY_OF_MONTH, i * 2);
-                    days[i + 6] = day;
-                }
-                dpd.setSelectableDays(days);
-            }
+            dpd.setSelectableDays(days);
 
             dpd.setOnCancelListener(dialog -> {
                 Log.d("DatePickerDialog", "Dialog was cancelled");
                 dpd = null;
             });
-            dpd.show(getSupportFragmentManager(), "Datepickerdialog");
-        });
+
+            // Add the date picker dialog to the FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.datePickerContainer, dpd)
+                    .addToBackStack(null)
+                    .commit();
+        }
     }
 
     @Override
